6f1e206d8ad967a8b13aebdc9ba2c8576c66b71d
luvi-kh <nalsengom@daum.net>
2024-09-23 20:51:03 +0900

    OrderPage, Type, Products 생성
    1.주문 페이지 UI 생성하기
    2. 상품 이미지 가져오기

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import SummaryPage from './pages/SummaryPage';
+import OrderPage from './pages/OrderPage';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ padding: '4rem'}}>
+      <OrderPage />
       <SummaryPage />
     </div>
+
   );
 }
 
